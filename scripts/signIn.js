da6102fa4d4b0f991108233dbb474b991f5b58ac
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-14 14:32:06 +0300

    Update add a method that loads all the users in local storage in the signin.js file

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -1,4 +1,22 @@
 const signinform = document.querySelector('.signinform');
-const usermail = document.querySelector('.usermail');
-const userPwd = document.querySelector('.userPwd');
+const usermail = document.querySelector('.usermail').value;
+const userPwd = document.querySelector('.userPwd').value;
 const userSignInBTN = document.querySelector('.userSignInBTN');
+
+const RelworkxUsers = [];
+
+const getUsersFromLocalStorage = () => {
+  if(localStorage.getItem('RelworxUser')) {
+    users = JSON.parse(localStorage.getItem('RelworxUser'));
+  }
+
+  users.forEach((user) => {
+    RelworkxUsers.push(user);
+  })
+  
+  return RelworkxUsers;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  getUsersFromLocalStorage();
+})
