ace3ecf3b9a5c531679e714f7e3f85eeab763477
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-14 14:26:38 +0300

    Update included:   - Classes for signin form elemens to attach event listenrs to   - created the signin.js file and setup signin form variables   - Linked the signin.js file to the project

---
+++ scripts/signIn.js
@@ -0,0 +1,4 @@
+const signinform = document.querySelector('.signinform');
+const usermail = document.querySelector('.usermail');
+const userPwd = document.querySelector('.userPwd');
+const userSignInBTN = document.querySelector('.userSignInBTN');
